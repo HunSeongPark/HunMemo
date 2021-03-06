b717bf9e2fb018e7603ea34ee6b944c97a5d2c06
HunSeong Park <gnstjd980831@naver.com>
2021-03-06 18:43:41 +0900

    메모 보기 데이터 전달

--- HunMemo/DetailViewController.swift
+++ HunMemo/DetailViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+  
+  var memo: Memo?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,10 +41,22 @@ extension DetailViewController: UITableViewDataSource {
       let cell = tableView.dequeueReusableCell(
         withIdentifier: "memoCell", for: indexPath)
       
+      cell.textLabel?.text = memo?.content
+      
       return cell
     case 1:
       let cell = tableView.dequeueReusableCell(
         withIdentifier: "dateCell", for: indexPath)
+      
+      let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+      }()
+      
+      cell.textLabel?.text = formatter.string(for: memo?.insertDate)
       
       return cell
     default:
