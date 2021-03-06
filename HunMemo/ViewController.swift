3df65d0b969b14047dbf7fc393b7e03b27d949a2
HunSeong Park <gnstjd980831@naver.com>
2021-03-06 15:21:14 +0900

    프로젝트 설정

---
+++ HunMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HunMemo
+//
+//  Created by 박훈성 on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
